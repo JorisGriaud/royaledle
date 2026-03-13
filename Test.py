cfab3567d566a5299d0c22f1eafb8d0a93df9509
Evan49752 <evan_delaunay@reseau.eseo.fr>
2026-03-13 08:09:11 +0100

    bon

--- Test.py
+++ Test.py
@@ -1,1 +1,127 @@
-print("test")
+from tkinter import *
+from PIL import Image, ImageTk
+
+fenetre = Tk()
+fenetre.title("RoyaleDLE")
+fenetre.geometry("1525x800") 
+fenetre.iconbitmap("logo.ico")
+
+# Charger l'image originale
+#image_originale = Image.open("fond.png")
+image_originale = Image.open("unnamed.png")    
+photo_ref = None
+
+# Dimensions du rectangle
+largeur_rect = 400
+hauteur_rect = 100
+hauteur_fixe = 100
+
+def classic(): #programme manu
+    pass
+
+def emoji(): #programme evan
+    pass
+
+def description():
+    pass
+
+image_bouton1 = Image.open("classic.png").resize((largeur_rect, hauteur_rect))  # Redimensionner à la taille du bouton
+photo_bouton1 = ImageTk.PhotoImage(image_bouton1)
+bouton1 = Button(fenetre, image=photo_bouton1, relief="flat", borderwidth=0, command=lambda: classic)
+bouton1.image = photo_bouton1 
+
+image_bouton2 = Image.open("emoji.png").resize((largeur_rect, hauteur_rect))  # Redimensionner à la taille du bouton
+photo_bouton2 = ImageTk.PhotoImage(image_bouton2)
+bouton2 = Button(fenetre, image=photo_bouton2, relief="flat", borderwidth=0, command=lambda: emoji)
+bouton2.image = photo_bouton2
+
+image_bouton3 = Image.open("description.png").resize((largeur_rect, hauteur_rect))  # Redimensionner à la taille du bouton
+photo_bouton3 = ImageTk.PhotoImage(image_bouton3)
+bouton3 = Button(fenetre, image=photo_bouton3, relief="flat", borderwidth=0, command=lambda: description)
+bouton3.image = photo_bouton3
+
+image_bouton4 = Image.open("descdeb.png").resize((largeur_rect, hauteur_rect))  # Redimensionner à la taille du bouton
+photo_bouton4 = ImageTk.PhotoImage(image_bouton4)
+bouton4 = Button(fenetre, image=photo_bouton4, relief="flat", borderwidth=0, command=lambda: classic)
+bouton4.image = photo_bouton4
+
+image_bouton5 = Image.open("emojdeb.png").resize((largeur_rect, hauteur_rect))  # Redimensionner à la taille du bouton
+photo_bouton5 = ImageTk.PhotoImage(image_bouton5)
+bouton5 = Button(fenetre, image=photo_bouton5, relief="flat", borderwidth=0, command=lambda: emoji)
+bouton5.image = photo_bouton5
+
+image_bouton6 = Image.open("classdeb.png").resize((largeur_rect, hauteur_rect))  # Redimensionner à la taille du bouton
+photo_bouton6 = ImageTk.PhotoImage(image_bouton6)
+bouton6 = Button(fenetre, image=photo_bouton6, relief="flat", borderwidth=0, command=lambda: description)
+bouton6.image = photo_bouton6
+
+logo = Image.open("cdetoutebeaute.png").resize((largeur_rect, hauteur_rect+200))  # Redimensionner à la taille du logo
+photo_logo = ImageTk.PhotoImage(logo)
+
+# Créer un Label avec l'image
+label_image = Label(fenetre, image=photo_logo,relief="flat", borderwidth=0)
+label_image.image = photo_logo  # Référence pour éviter la disparition
+
+
+def redimensionner_canvas(event):
+    global photo_ref
+    # Redimensionner l'image
+    image_redim = image_originale.resize((event.width, event.height))
+    photo_ref = ImageTk.PhotoImage(image_redim)
+    
+    # Effacer et redessiner
+    canvas.delete("all")
+    canvas.create_image(0, 0, image=photo_ref, anchor=NW)
+
+    x1 = (event.width - largeur_rect) // 2
+    y1 = hauteur_fixe    
+    
+    label_image.place(x=x1, y=y1-50,width=largeur_rect, height=hauteur_rect)  # Position du logo
+    label_image.lift()
+
+    # Placer le bouton et le mettre au-dessus
+    bouton1.place(x=x1-75, y=y1, width=largeur_rect, height=hauteur_rect)
+    bouton1.lift()
+
+    bouton2.place(x=x1, y=y1, width=largeur_rect, height=hauteur_rect)
+    bouton2.lift()
+
+    bouton3.place(x=x1+75, y=y1, width=largeur_rect, height=hauteur_rect)
+    bouton3.lift()
+
+    
+
+
+def redimensionner_canvas_debut(event):
+    global photo_ref
+    # Redimensionner l'image
+    image_redim = image_originale.resize((event.width, event.height))
+    photo_ref = ImageTk.PhotoImage(image_redim)
+    
+    # Effacer et redessiner
+    canvas.delete("all")
+    canvas.create_image(0, 0, image=photo_ref, anchor=NW)
+
+    x1 = (event.width - largeur_rect) // 2
+    y1 = hauteur_fixe    
+   
+    label_image.place(x=x1, y=y1-50,width=largeur_rect, height=hauteur_rect)  # Position du logo
+    label_image.lift()
+
+    # Placer le bouton et le mettre au-dessus
+    bouton4.place(x=x1, y=y1+100, width=largeur_rect, height=hauteur_rect)
+    bouton4.lift()
+
+    bouton5.place(x=x1, y=y1+225, width=largeur_rect, height=hauteur_rect)
+    bouton5.lift()
+
+    bouton6.place(x=x1, y=y1+350, width=largeur_rect, height=hauteur_rect)
+    bouton6.lift()
+
+    
+# Canvas
+canvas = Canvas(fenetre, highlightthickness=0)
+canvas.pack(fill=BOTH, expand=True)
+canvas.bind("<Configure>", redimensionner_canvas_debut)
+
+fenetre.mainloop()
