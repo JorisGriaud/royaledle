c73686437b95d568ed6f79e08b7d7b92936c7144
Joris Griaud <joris.griaud@reseau.eseo.fr>
2026-03-17 10:29:17 +0100

    Try to add emeji

---
+++ dl_image.py
@@ -0,0 +1,17 @@
+from Cards import Cards
+
+import requests
+
+cards = Cards()
+
+def DownloadFile(url, name):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url)
+    f = open(local_filename, 'wb')
+    for chunk in r.iter_content(chunk_size=512 * 1024):
+        if chunk: # filter out keep-alive new chunks
+            f.write(chunk)
+    f.close()
+    return
+
+DownloadFile("https://api-assets.clashroyale.com/cards/300/Ubu0oUl8tZkusnkZf8Xv9Vno5IO29Y-jbZ4fhoNJ5oc.png")
