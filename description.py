348e5ad9623113bb7a23e814a690bde7b877118f
Emmanuel8VR0C2JR <emmanuel.grossemy@reseau.eseo.fr>
2026-03-20 09:50:11 +0100

    texte a troues et progression

--- description.py
+++ description.py
@@ -4,20 +4,18 @@ cards = Cards()
 def choixcarte():
    carte=randint(0,cards.get_number_of_cards()-1)
    return carte
-def choixdescription():
-   carte=choixcarte()
+
+def choixdescription(carte=choixcarte()):
    card= cards.get_card_by_id(carte)
    description=card.get_description()
    return description
 
 def texteatroues():
     carte=choixcarte()
-    b=str(cards.get_card_by_id(carte)).lower()
-    print(b)
-    description = choixdescription()
+    b=str(cards.get_card_by_id(carte).get_name()).lower()
+    description = choixdescription(carte)
     description=str(description)
     tab=description.split(sep=" ")
-    print(tab)
     t=[0 for _ in range(len(tab))]
     n=4
     check=[]
@@ -27,8 +25,29 @@ def texteatroues():
            a=randint(0,len(tab)-1)
        check.append(a)
        t[a]=tab[a]
+    for i in range (len(t)):
+       if t[i]==0:
+          l=["_" for _ in range(len((tab[i])))]
+          t[i]=l
+    t.append(carte)
     return t
-print(texteatroues())
+
+def trouve():
+ pass
+
+def progression():
+   t=texteatroues()
+   description=choixdescription(t[-1])
+   description=str(description)
+   tab=description.split(sep=" ")
+   t.pop(-1)
+   while trouve==False:
+      r=randint(0,len(t)-1)
+      if t[r] is 
+         
+      
+   
+progression()
 
 
        
